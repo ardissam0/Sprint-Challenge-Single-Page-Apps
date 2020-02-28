a407f8d101c9c5ee3ca8e07f61bf82682d39e4bb
Sam Ardis <ardis.productions@gmail.com>
2020-02-28 11:38:49 -0500

    Added charactercard and characterlist starting code

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({ character }) {
+  return (
+    <div>
+      <Card>
+        <CardImg top width='100%' src={character.image} alt='Card image' />
+        <CardBody>
+          <CardTitle>
+            <h2>{character.name}</h2>
+          </CardTitle>
+          <CardSubtitle>
+            <span>Status: </span>
+            <p>{character.gender}</p>
+          </CardSubtitle>
+          <CardSubtitle>
+            <span>Episodes: </span>
+            <p>{character.episodes.length}</p>
+          </CardSubtitle>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
